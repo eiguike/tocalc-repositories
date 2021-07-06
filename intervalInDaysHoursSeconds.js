350bcd922c9cd3bdc29d67fd5cb9e855c435e5b2
rick <rick.eihara@gmail.com>
2021-07-06 17:07:47 -0300

    feat: intervalInDaysHoursSeconds

---
+++ intervalInDaysHoursSeconds.js
@@ -0,0 +1,27 @@
+number("firstDate");
+number("secondDate");
+
+calculation((firstDateString, secondDateString) => {
+  const oneDayInSeconds = 24 * 60 * 60;
+  const hoursInSeconds = 60 * 60;
+
+  const firstDate = Math.round(Math.min(
+    new Date(firstDateString).getTime(),
+    new Date(secondDateString).getTime()
+  ) / 1000);
+  const secondDate = Math.round(Math.max(
+    new Date(firstDateString).getTime(),
+    new Date(secondDateString).getTime()
+  ) / 1000);
+
+  const daysInSeconds = secondDate - firstDate;
+  const days = daysInSeconds / oneDayInSeconds;
+  const hours = daysInSeconds / hoursInSeconds;
+  const seconds = daysInSeconds;
+
+  return [
+    { result: `INTERVALO EM DIAS: ${days}`},
+    { result: `INTERVALO EM HORAS: ${hours}`},
+    { result: `INTERVALO EM SEGUNDOS: ${seconds}`},
+  ];
+});
